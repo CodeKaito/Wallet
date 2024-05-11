77bab5991b023d8376ed4752dd6a5e642ed242f2
CodeKaito <jedecinco@gmail.com>
2024-05-11 10:53:22 +0200

    update

---
+++ frontend/src/data/index.js
@@ -0,0 +1,33 @@
+export const DataTransactions = [
+  {
+    id: 1,
+    date: "2022-05-01",
+    amount: `110€`,
+    type: "Deposit",
+    note: "Deposito nel conto in banca",
+  },
+  {
+    id: 2,
+    date: "2022-05-02",
+    amount: `150€`,
+    type: "Withdrawal",
+  },
+  {
+    id: 3,
+    date: "2022-05-03",
+    amount: `20€`,
+    type: "Transfer",
+  },
+  {
+    id: 4,
+    date: "2022-05-04",
+    amount: `120€`,
+    type: "Deposit",
+  },
+  {
+    id: 5,
+    date: "2022-05-05",
+    amount: `180€`,
+    type: "Withdrawal",
+  },
+];
--- frontend/src/pages/transaction/Transaction.jsx
+++ frontend/src/pages/transaction/Transaction.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { Header } from "../../components";
 import { DataGrid } from "@mui/x-data-grid";
+import { DataTransactions } from "../../data";
 
 const Transaction = () => {
   const columns = [
@@ -8,49 +9,16 @@ const Transaction = () => {
     { field: "date", headerName: "Date", width: 250 },
     { field: "amount", headerName: "Amount", width: 150 },
     { field: "type", headerName: "Type", width: 150 },
-    { field: "note", headerName: "Note", width: 450 },
+    { field: "note", headerName: "Note", width: 150 },
   ];
 
-  const rows = [
-    {
-      id: 1,
-      date: "2022-05-01",
-      amount: `110€`,
-      type: "Deposit",
-      note: "Deposito nel conto in banca",
-    },
-    {
-      id: 2,
-      date: "2022-05-02",
-      amount: `150€`,
-      type: "Withdrawal",
-    },
-    {
-      id: 3,
-      date: "2022-05-03",
-      amount: `20€`,
-      type: "Transfer",
-    },
-    {
-      id: 4,
-      date: "2022-05-04",
-      amount: `120€`,
-      type: "Deposit",
-    },
-    {
-      id: 5,
-      date: "2022-05-05",
-      amount: `180€`,
-      type: "Withdrawal",
-    },
-  ];
   return (
     <Box m="20px">
       <Box className="flex justify-between align-center">
         <Header title="Transactions" />
       </Box>
       <Box m="40px 0 0 0" height="75vh">
-        <DataGrid checkboxSelection rows={rows} columns={columns} />
+        <DataGrid checkboxSelection rows={DataTransactions} columns={columns} />
       </Box>
     </Box>
   );
